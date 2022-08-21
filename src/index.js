1a38b28d25633061070d7dc654c3a4074a9aca5e
Emil <codewithemil@gmail.com>
2022-08-21 16:19:42 -0500

    Master Branch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//CSS
+import "./index.css"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Setup Vars
+const firstBook = {
+    img: "https://m.media-amazon.com/images/I/61XTlRi7+2L._AC_UY218_.jpg",
+    title: "Blue Crab Finds a Home: A Book of Affirmations for Kids",
+    author: "Maurine Frank"
+};
+
+function BookList() {
+    return (
+        <section className="booklist">
+            <Book job = "programmer" />
+            <Book title = "random title" number = {22} />
+        </section>
+    )
+}
+
+const Book = (props) => {
+    return (
+        <article className = "book">
+            <img
+                src={img}
+                alt=""
+            />
+            <h1>{title}</h1>
+            <h4>{author}</h4>
+            <p>{props.job}</p>
+            <p>{props.title}</p>
+            <p>{props.number}</p>
+        </article>
+    )
+}
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
