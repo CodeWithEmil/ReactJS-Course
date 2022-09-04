6692dcec281a2b723a4bd1914ef71dc29e619b3c
Emil <codewithemil@gmail.com>
2022-09-04 01:13:34 -0500

    Learned how to convert an array to a React render. Will continue today but in the morning (its like 1 am rn)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+//!!! Video Time: 2:45:24
+
+
+
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -5,33 +9,38 @@ import ReactDOM from "react-dom";
 import "./index.css"
 
 //Setup Vars
-const firstBook = {
-    img: "https://m.media-amazon.com/images/I/61XTlRi7+2L._AC_UY218_.jpg",
-    title: "Blue Crab Finds a Home: A Book of Affirmations for Kids",
-    author: "Maurine Frank"
-};
+const books = [
 
-const secondBook = {
-    img: "https://images-na.ssl-images-amazon.com/images/I/41g9RfNVZBL._SX320_BO1,204,203,200_.jpg",
-    title: "Ugly Love: A Novel",
-    author: "Colleen Hoover",
-};
+    
+    {
+        img: "https://m.media-amazon.com/images/I/61XTlRi7+2L._AC_UY218_.jpg",
+        title: "Blue Crab Finds a Home: A Book of Affirmations for Kids",
+        author: "Maurine Frank"
+    },
+    {
+        img: "https://images-na.ssl-images-amazon.com/images/I/41g9RfNVZBL._SX320_BO1,204,203,200_.jpg",
+        title: "Ugly Love: A Novel",
+        author: "Colleen Hoover",
+    },
+]
 
+const names = ["john", "peter", "susan"];
+const newNames = names.map((name) => {
+    return <h1>{name}</h1>
+});
+console.log(newNames)
 
 //Code
 function BookList() {
     return (
         <section className="booklist">
-            <Book img = {firstBook.img} title = {firstBook.title} author = {firstBook.author}>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed in impedit voluptates pariatur ratione totam dolore ipsa numquam repellendus accusamus?</p>
-            </Book>
-            <Book img = {secondBook.img} title = {secondBook.title} author = {secondBook.author} />
+            {newNames}
         </section>
     )
 }
 
 const Book = (props) => {
-    const {img, title, author, children} = props;
+    const {img, title, author} = props;
     return (
         <article className = "book">
             <img
@@ -40,9 +49,13 @@ const Book = (props) => {
                 />
             <h1>{title}</h1>
             <h4>{author}</h4>
-            {children}
         </article>
     )
 }
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
+
+
+
+            // <Book img = {firstBook.img} title = {firstBook.title} author = {firstBook.author} />
+            // <Book img = {secondBook.img} title = {secondBook.title} author = {secondBook.author} />
