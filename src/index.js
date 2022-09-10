108d4b2807c25d2dd1253e15573dae971ced5f27
Emil <codewithemil@gmail.com>
2022-09-10 01:20:31 -0500

    Started to learn about a way in which I can destructure and pass props using the map method. I will now proceed to see it whilst using the Spread Operator instead

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//!!! Video Time: 2:45:24
+//!!! Video Time: 2:55:54
 
 
 
@@ -10,37 +10,44 @@ import "./index.css"
 
 //Setup Vars
 const books = [
-
-    
     {
+        id: 1,
         img: "https://m.media-amazon.com/images/I/61XTlRi7+2L._AC_UY218_.jpg",
         title: "Blue Crab Finds a Home: A Book of Affirmations for Kids",
-        author: "Maurine Frank"
+        author: "Maurine Frank",
     },
     {
+        id: 2,
         img: "https://images-na.ssl-images-amazon.com/images/I/41g9RfNVZBL._SX320_BO1,204,203,200_.jpg",
         title: "Ugly Love: A Novel",
         author: "Colleen Hoover",
     },
-]
+    {
+        id: 3,
+        img: "https://m.media-amazon.com/images/I/91Tb3rvyZEL._AC_UY218_.jpg",
+        title: "A Gentleman in Moscow: a Novel",
+        author: "Amor Towles",
+    },
+];
 
-const names = ["john", "peter", "susan"];
-const newNames = names.map((name) => {
-    return <h1>{name}</h1>
-});
-console.log(newNames)
+
 
 //Code
 function BookList() {
     return (
         <section className="booklist">
-            {newNames}
+            {books.map((book) => {
+                const {img, title, author} = book;
+                return (
+                    <Book key={book.id} bookProp={book} />
+                );
+            })}
         </section>
     )
 }
 
 const Book = (props) => {
-    const {img, title, author} = props;
+    const {img, title, author} = props.bookProp;
     return (
         <article className = "book">
             <img
